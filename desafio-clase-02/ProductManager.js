aa7e82ce99b0247fbf92f5f31f8ed5c063c3b8fe
santimolina <molan_24@hotmail.com>
2023-07-27 20:26:42 -0300

    entrega clase 02

---
+++ desafio-clase-02/ProductManager.js
@@ -0,0 +1,52 @@
+
+class ProductManager {
+    #_products
+    constructor() {
+        this.#_products = [] 
+    }
+    
+    getProducts() {
+        return this.#_products
+    }
+
+    addProduct(product) {
+        if (!product.title||!product.description||!product.price||!product.thumbnail||!product.code||!product.stock) {
+            return '[ERR] No estan informados todos los campos necesarios para añadir el producto'
+        }
+        
+        const found = this.#_products.find(item => item.code === product.code)
+        if(found) {
+            return '[ERR] Codigo invalido, ya existente.'
+        } else {
+            this.#_products.push({id: this.#getNextID(), ...product})
+            return 'Agregado correctamente'
+        }
+    }                                                        
+
+    #getNextID() {
+        return this.#_products.length+1;
+    }
+
+}
+
+const pm = new ProductManager()
+
+console.log(pm.addProduct({
+                title: 'fideos',
+                description: 'fideos marolio',
+                price: 120,
+                thumbnail: 1500,
+                code: 200,
+                stock: 15
+            }))
+console.log(pm.addProduct({
+                title: 'queso',
+                description: 'queso rallado',
+                price: 100,
+                thumbnail: 1500,
+                code: 201,
+                stock: 10
+            }))
+
+
+console.log(pm.getProducts())
